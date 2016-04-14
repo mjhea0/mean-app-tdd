9984308268b5b12e49b1d13be36f753cc4733b51
Michael Herman <hermanmu@gmail.com>
2016-04-14 10:47:40 -0600

    added production database to config

--- src/_config.js
+++ src/_config.js
@@ -3,7 +3,7 @@ var config = {};
 config.mongoURI = {
   test: 'mongodb://localhost/first-mean-app-testing',
   development: 'mongodb://localhost/first-mean-app',
-  production: process.env.mongoURI
+  production: process.env.MONGODB_URI
 };
 
 module.exports = config;
